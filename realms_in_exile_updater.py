56e4bb29fc791d6a052ac78e9e22021866bb17ee
hansnery <hansnery@gmail.com>
2023-11-02 14:32:08 -0400

    Initial commit

---
+++ realms_in_exile_updater.py
@@ -0,0 +1,92 @@
+import os
+import requests
+from tqdm import tqdm
+import zipfile
+
+BASE_URL = "https://storage.googleapis.com/realms-in-exile/updater/"
+VERSION_FILE = "version.txt"
+MOD_FILE = "files.zip"
+
+def download_file(filename):
+    file_path = os.path.join(script_directory, filename)
+    url = BASE_URL + filename
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get('content-length', 0))
+    block_size = 1024
+    t = tqdm(total=total_size, unit='B', unit_scale=True, desc=filename)
+    with open(file_path, 'wb') as f:
+        for data in response.iter_content(block_size):
+            t.update(len(data))
+            f.write(data)
+    t.close()
+
+def extract_with_progress(zip_path, extract_path):
+    with zipfile.ZipFile(zip_path, 'r') as zf:
+        # Get the total number of entries within the ZIP file
+        total_files = len(zf.infolist())
+        with tqdm(total=total_files, unit='file', desc="Extracting") as t:
+            for member in zf.infolist():
+                zf.extract(member, extract_path)
+                t.update(1)
+
+def get_local_version():
+    with open(os.path.join(script_directory, VERSION_FILE), 'r') as f:
+        return f.read().strip()
+
+def get_online_version():
+    try:
+        response = requests.get(BASE_URL + VERSION_FILE)
+        response.raise_for_status()  # This will raise an error for 4xx and 5xx status codes
+        return response.text.strip()
+    except requests.RequestException as e:
+        print(f"Error fetching online version: {e}")
+        return None
+
+def cleanup_and_retry():
+    os.remove(os.path.join(script_directory, MOD_FILE))
+    choice = input("An error occurred. Would you like to start over? (Y/N): ").upper()
+    if choice == 'Y':
+        main()
+
+def main():
+    global script_directory
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+
+    try:
+        local_version = get_local_version() if os.path.exists(os.path.join(script_directory, VERSION_FILE)) else None
+        online_version = get_online_version()
+
+        if not local_version:
+            if online_version is None:
+                print("Unable to check for updates at this time.")
+                return
+            print(f"'Age of the Ring: Realms in Exile' is not installed. Installing version {online_version}...")
+            download_file(VERSION_FILE)
+            download_file(MOD_FILE)
+            print("Installing files...")
+            extract_with_progress(os.path.join(script_directory, MOD_FILE), script_directory)
+            os.remove(os.path.join(script_directory, MOD_FILE))
+            print(f"'Age of the Ring: Realms in Exile' version {online_version} was installed successfully!")
+            return
+
+        if online_version is None:
+            print("Unable to check for updates at this time.")
+            return
+
+        if local_version != online_version:
+            print(f"Your version of 'Age of the Ring: Realms in Exile' is {local_version}. Updating to version {online_version}...")
+            download_file(VERSION_FILE)
+            download_file(MOD_FILE)
+            print("Updating files...")
+            extract_with_progress(os.path.join(script_directory, MOD_FILE), script_directory)
+            os.remove(os.path.join(script_directory, MOD_FILE))
+            print(f"'Age of the Ring: Realms in Exile' was updated to version {online_version} successfully!")
+        else:
+            print(f"You have the latest version of 'Age of the Ring: Realms in Exile' ({local_version}).")
+
+    except Exception as e:
+        print(f"Error: {e}")
+        cleanup_and_retry()
+
+if __name__ == "__main__":
+    main()
